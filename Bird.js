01591e1050a08b8754b13a7fa09f267774868cf7
Avneet-123 <64260638+Avneet-123@users.noreply.github.com>
2020-06-03 10:09:04 +0530

    Update Bird.js

--- Bird.js
+++ Bird.js
@@ -1,7 +1,7 @@
 class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
-    this.image = loadImage("sprites/harry.jpg");
+    this.image = loadImage("harry.jpg");
     this.smokeImage = loadImage("sprites/smoke.png");
     this.trajectory =[];
   }
